088d8621319116808aacc80012c22ea5e83d1f1a
Joaquim Bolós Fernández <bolosfernandez@hotmail.es>
2023-01-13 13:41:59 +0000

    Task 2 - Milestone 4

--- airbnb-property-listings/modelling.py
+++ airbnb-property-listings/modelling.py
@@ -6,18 +6,7 @@ from sklearn.linear_model import SGDRegressor
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-
-def mse_loss(y_hat, labels): # define the criterion (loss function)
-    errors = y_hat - labels ## calculate the errors
-    squared_errors = errors ** 2 ## square the errors
-    mean_squared_error = sum(squared_errors) / len(squared_errors) ## calculate the mean 
-    return mean_squared_error
-
-def calculate_loss(model, X, y):
-    return mse_loss(
-        model.predict(X),
-        y
-    )
+from sklearn.preprocessing import StandardScaler
 
 X, y = load_airbnb()
 
@@ -29,16 +18,21 @@ np.random.seed(10)
 X['guests'] = X['guests'].str.replace('\'','').astype(np.float64)
 X['bedrooms'] = X['bedrooms'].str.replace('\'','').astype(np.float64)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+std = StandardScaler()
+X = std.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=12)
 X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5)
 
 linear_regression_model_SDGRegr = SGDRegressor()
 
 linear_regression_model = linear_model.LinearRegression()
 
-model = linear_regression_model.fit(X_train, y_train)
+model = linear_regression_model_SDGRegr.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
+y_pred_train = model.predict(X_train)
 
 print("Number of samples in:")
 print(f" - Training: {len(y_train)}")
@@ -47,20 +41,40 @@ print(f" - Testing: {len(y_test)}")
 
 print(" ")
 
-print("Loss after fit:")
-print(f"Training loss after fit: {calculate_loss(model, X_train, y_train)}")
-print(f"Validation loss after fit: {calculate_loss(model, X_validation, y_validation)}")
-print(f"Test loss after fit: {calculate_loss(model, X_test, y_test)}")
-
-print(" ")
-
 print('final weights:', model.coef_)
 print('final bias:', model.intercept_)
 
 print(" ")
+print("Testing Metrics")
 
 print("MSE: ")
 print(metrics.mean_squared_error(y_test, y_pred))
+
+print("RMSE: ")
+print(metrics.mean_squared_error(y_test, y_pred, squared=False))
+
+print("MAE: ")
+print(metrics.mean_absolute_error(y_test, y_pred))
+
+print("R^2: ")
+print(metrics.r2_score(y_test, y_pred))
+
+
+print(" ")
+print("Training Metrics")
+
+print("MSE: ")
+print(metrics.mean_squared_error(y_train, y_pred_train))
+
+print("RMSE: ")
+print(metrics.mean_squared_error(y_train, y_pred_train, squared=False))
+
+print("MAE: ")
+print(metrics.mean_absolute_error(y_train, y_pred_train))
+
+print("R^2: ")
+print(metrics.r2_score(y_train, y_pred_train))
+
 # %%
 y_pred[0:5]
 
