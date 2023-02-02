118e567f09d6a552c0e4d088e45233a80aecfe6c
Joaquim Bolós Fernández <bolosfernandez@hotmail.es>
2023-02-02 13:46:47 +0000

    Notebook of Milestone 6

--- airbnb-property-listings/neural_network.py
+++ airbnb-property-listings/neural_network.py
@@ -4,6 +4,7 @@ from tabular_data import load_airbnb
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
+import torch.nn.functional as F
 
 # Preprocessing the DataSet
 X, y = load_airbnb()
@@ -11,8 +12,8 @@ X.drop(532, axis=0, inplace=True)
 y.drop(532, axis=0, inplace=True)
 X['guests'] = X['guests'].str.replace('\'','').astype(np.float64)
 X['bedrooms'] = X['bedrooms'].str.replace('\'','').astype(np.float64)
-
 class AirbnbDataset(torch.utils.data.Dataset):
+
     def __init__(self):
         super().__init__()
         self.X, self.y = X , y
@@ -24,29 +25,55 @@ class AirbnbDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.X)
+    
 
-AirbnbNightlyPriceImageDataset = AirbnbDataset()
-print(AirbnbNightlyPriceImageDataset[10])
-print(len(AirbnbNightlyPriceImageDataset))
+dataset = AirbnbDataset()
 
-DataLoader(AirbnbNightlyPriceImageDataset, batch_size=16, shuffle=True)
+# print(dataset[10])
+# print(len(dataset))
 
-BATCH_SIZE = 64
+batch_size = 20
 
-train_dataset, validation_dataset, test_dataset = random_split(range(30), [0.7, 0.15, 0.15], generator=torch.Generator().manual_seed(42))
+train_dataset, validation_dataset, test_dataset = random_split(dataset, [0.7, 0.15, 0.15], generator=torch.Generator().manual_seed(42))
 
-dataloaders = {
-    "train": torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-        pin_memory=torch.cuda.is_available(),
-    ),
-    "validation": torch.utils.data.DataLoader(
-        validation_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=torch.cuda.is_available()
-    ),
-    "test": torch.utils.data.DataLoader(
-        test_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=torch.cuda.is_available()
-    ),
-}
+train_loader=DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validation_loader=DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+test_loader=DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+# train_example = next(iter(train_loader))
+# features, labels = train_example
+# features = features.to(torch.float64)
+# labels = labels.to(torch.float64)
+# print(features.dtype, labels.dtype)
+
+# print(features)
+class LinearRegression(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # Initialise the Parameters
+        self.linear_layer = torch.nn.Linear(11,1)
+
+    def forward(self, features):
+        # Use the layers to process the features
+        return self.linear_layer(features)
+
+model = LinearRegression()
+# print(model(features))
+
+def train(model, dataloader, epochs=10):
+    for epoch in range(epochs):
+        for batch in dataloader:
+            features, labels = batch
+            prediction = model(features)
+            loss = F.mse_loss(prediction, labels)
+            loss.backward()
+            print(loss)
+    return
+
+train(model, train_loader)
+# %%
+
+# %%
+X.columns
 # %%
