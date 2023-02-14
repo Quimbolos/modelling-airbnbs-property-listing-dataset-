b72656d91078c9ec8031aad29fb71a8809dab4c0
Joaquim Bolós Fernández <bolosfernandez@hotmail.es>
2023-02-14 12:01:24 +0000

    Milestone 6 - Task 5

--- airbnb-property-listings/modelling_neural_network.py
+++ airbnb-property-listings/modelling_neural_network.py
@@ -54,20 +54,40 @@ test_loader=DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 from torch.utils.tensorboard import SummaryWriter
 torch.manual_seed(10)
 
+# yaml file content
+''' 
+optimiser: SGD
+lr: 0.001
+hidden_layer_width: 32
+depth: 5
+'''
+# Define function get_nn_config()
+import yaml
+def get_nn_config():
+    with open('nn_config.yaml', 'r') as stream:
+    # Converts yaml document to python object
+        dictionary=yaml.safe_load(stream)
+    return dictionary
+
+# Retrieve config dictionary
+nn_config = get_nn_config()
+
 # Neural Networks Model - Updated with more Layers
 class NeuralNetwork(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
         # Initialise the Parameters
-        self.layers = torch.nn.Sequential( # Update Model with more Layers
-        torch.nn.Linear(11, 20),
-        torch.nn.ReLU(),
-        torch.nn.Linear(20, 10),
-        torch.nn.ReLU(),
-        torch.nn.Linear(10, 1)
-        )
+        #self.linear_layer = torch.nn.Linear(11,1) # 11 features, 1 label
 
+        self.layers = torch.nn.Sequential()
+        self.layers.add_module("Input Layer", torch.nn.Linear(11, nn_config['hidden_layer_width'])) # Input layer
+        self.layers.add_module("ReLU", torch.nn.ReLU())
+        for i in range(nn_config['depth'] - 2): #  The input and the first linear layer are already taken into account
+            self.layers.add_module("Hidden Layer", torch.nn.Linear(nn_config['hidden_layer_width'], nn_config['hidden_layer_width'])) # Hidden Layer
+            self.layers.add_module("Hidden ReLU", torch.nn.ReLU())
+        self.layers.add_module("Output Layer", torch.nn.Linear(nn_config['hidden_layer_width'], 1))# output layer
+    
     def forward(self, features):
         # Use the layers to process the features
         return self.layers(features)
@@ -75,10 +95,18 @@ class NeuralNetwork(torch.nn.Module):
 model = NeuralNetwork()
 loss_fn = torch.nn.MSELoss()
 
-# Train function with Optimiser and Tensorboard
-def train(model, dataloader, epochs=30):
+# Train function with Tensorboard
+def train(model, dataloader, test_dl, epochs=20):
+    """pass a train and validaton dataloader """
+    # Set optimiser with lr from nn_config
+    if nn_config['optimiser'] == "SGD":
+        optimiser = torch.optim.SGD(model.parameters(), lr=nn_config['lr'])
 
-    optimiser = torch.optim.SGD(model.parameters(), lr=0.00001)
+    elif nn_config['optimiser'] == "Adam":
+        optimiser = torch.optim.Adam(model.parameters(), lr=nn_config['lr'])
+
+    elif nn_config['optimiser'] == "Adagrad":
+        optimiser = torch.optim.Adagrad(model.parameters(), lr=nn_config['lr'])
 
     writer = SummaryWriter()
 
@@ -95,17 +123,26 @@ def train(model, dataloader, epochs=30):
             optimiser.step() # Optimiser step
             optimiser.zero_grad()
             ls = loss.item()
-            print("Loss", ls)
             batch_idx += 1
             current_loss = current_loss + ls
-            writer.add_scalar("Loss",ls, epoch)
+            # writer.add_scalar("Loss - Task 4",ls, epoch)
         
         # print (f"currentnt loss {current_loss} and batch index {batch_idx}")
         # print(f'Loss after mini-batch  ({epoch + 1} : {current_loss // batch_idx}')
-        # writer.add_scalar('loss',current_loss / batch_idx , epoch)
-            
+        writer.add_scalar('loss',current_loss / batch_idx , epoch)
+        print("Loss", current_loss / batch_idx)
+        batch_idx = 0
+        current_loss = 0.0
+        for i, (inputs, targets) in enumerate(test_dl):
+        # evaluate the model on the test set
+            inputs = inputs.to(torch.float32) # Convert  into the right format
+            targets = targets.to(torch.float32) # Convert  into the right format
+            yhat = model(inputs)
+            loss = loss_fn(yhat,targets)
+            ls = loss.item()
+            batch_idx += 1
+            current_loss = current_loss + ls
         
-train(model,train_loader)
-
+        writer.add_scalar('loss', current_loss)
 # %%
 # %%
